762db183bb9b772f97f32c2d50f9dbcc749abb64
Greg Albrecht <github.com@undef.net>
2022-05-25 12:48:08 -0700

    format fix

--- aiscot/classes.py
+++ aiscot/classes.py
@@ -19,6 +19,7 @@ __author__ = "Greg Albrecht W2GMD <oss@undef.net>"
 __copyright__ = "Copyright 2022 Greg Albrecht"
 __license__ = "Apache License, Version 2.0"
 
+
 # pylint: disable=too-many-instance-attributes
 class AISNetworkClient(asyncio.Protocol):
 
